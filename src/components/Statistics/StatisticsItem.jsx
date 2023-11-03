e8fc3299bd45cbd929f897a6a0e784610c0bf758
liddiia <liddabojko@gmail.com>
2023-11-04 01:31:14 +0200

    update

--- src/components/Statistics/StatisticsItem.jsx
+++ src/components/Statistics/StatisticsItem.jsx
@@ -1,7 +1,7 @@
 import css from './Statistics.module.css';
 import { getRandomHexColor } from './Stats';
-export const StatisticsItem = ({key, label, percentage})=>{return(
-	<li className={css.item} key ={key} style={{
+export const StatisticsItem = ({label, percentage})=>{return(
+	<li className={css.item} style={{
         backgroundColor: getRandomHexColor() }}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
